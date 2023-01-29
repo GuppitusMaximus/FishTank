d6396d0a866c7fa78121d7a090027537e7202b88
guppy <Guppitus@pm.me>
2023-01-29 12:55:16 -0500

    add button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const requestOptions = {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+    },
+    body: JSON.stringify({name: "Apples", color: 'red'})
+  };
+  const fruit = () => {
+    fetch('https://4j6jpt6px8.execute-api.us-east-1.amazonaws.com/helloworld', requestOptions)
+        .then(response => response.json())
+        .then((json) => {
+          console.log(json);
+        })
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +27,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={fruit}>Fruit</button>
         <a
           className="App-link"
           href="https://reactjs.org"
